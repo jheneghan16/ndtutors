3fb7aa495d5abaae3709c2c27f99972417fd8f91
jamie heneghan <jheneghan16@gmail.com>
2020-07-02 17:28:29 -0500

    In class edits

--- src/app/common/app.module.js
+++ src/app/common/app.module.js
@@ -1,4 +1,11 @@
 angular
     .module('common', [
         'ui.router'
-]);
+])
+
+.config(function ($locationProvider, $mdThemingProvider, ParseProvider) {
+    var MY_PARSE_APP_ID = 'gQyY6OXvKSw3jr6QeIoewjcNM8hYnNvWjKKwX9KX';
+    var MY_PARSE_JS_KEY = 'Ubvews9hpPAhLZVgFgTk3yYm1IvwZXKet7p5CyJC';
+    ParseProvider.initialize(MY_PARSE_APP_ID, MY_PARSE_JS_KEY);
+    ParseProvider.serverURL = 'https://parseapi.back4app.com/';
+});
