b636716158f4d8518e210429a26660eb5eff5f7b
jamie heneghan <jheneghan16@gmail.com>
2020-07-06 03:19:45 -0500

    things are working

--- src/app/common/app.module.js
+++ src/app/common/app.module.js
@@ -1,9 +1,10 @@
 angular
     .module('common', [
-        'ui.router'
+        'ui.router',
+        'ngParse'
 ])
 
-.config(function ($locationProvider, $mdThemingProvider, ParseProvider) {
+.config(function ($locationProvider, ParseProvider) {
     var MY_PARSE_APP_ID = 'gQyY6OXvKSw3jr6QeIoewjcNM8hYnNvWjKKwX9KX';
     var MY_PARSE_JS_KEY = 'Ubvews9hpPAhLZVgFgTk3yYm1IvwZXKet7p5CyJC';
     ParseProvider.initialize(MY_PARSE_APP_ID, MY_PARSE_JS_KEY);
