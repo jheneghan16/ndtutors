7f9eb147797325b3f9417272c0d67d5c4a19fcd0
jamie heneghan <jheneghan16@gmail.com>
2020-07-20 17:38:26 -0500

    Basic sendgrid functionality done

--- src/app/components/tutors/appt-list-elem/appt-list-elem.controller.js
+++ src/app/components/tutors/appt-list-elem/appt-list-elem.controller.js
@@ -1,6 +1,24 @@
 function ApptListElemController (ApptModel) {
 	const ctrl = this;
 	ctrl.updateAppt = ApptModel.updateAppt;
+
+	ctrl.emailToSend = {}
+
+	//var receiverEmail = ctrl.apptelem.tutorEmail;
+
+	ctrl.params = {
+		toEmail: "",
+		subject: "Someone signed up for your appointment",
+		body: " Someone signed up for your appointment "
+	}
+
+	ctrl.sendTutorEmail = function(params) {
+		console.log(params);
+		Parse.Cloud.run("sendgridEmail", params).then( function() {
+		console.log(response);
+	});
+	}
+
 }
 
 // apptList element controller
