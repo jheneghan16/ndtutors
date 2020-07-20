839fbc2a581ad0f12ad8ecce9a122c1382000fd1
jamie heneghan <jheneghan16@gmail.com>
2020-07-19 23:53:16 -0500

    Added make new appt

--- src/app/components/tutors/for-tutors/for-tutors.controller.js
+++ src/app/components/tutors/for-tutors/for-tutors.controller.js
@@ -1,9 +1,28 @@
 function forTutorsController (ApptModel, AuthService) {
    // So we can reference the same this at lower levels.     
     var ctrl = this;
-    ctrl.logout = AuthService.logout;
+	ctrl.logout = AuthService.logout;
+	ctrl.newappt = ApptModel.New();
+	ctrl.update = ApptModel.updateAppt;
+	ctrl.showForm = true;
+	
+	/*ctrl.newappt = {
+		'tutorName' : '',
+		'tutorName' : '',
+		'tutorEmail' : '',
+		'apptTime' : '',
+		'isAvail' : '',
+		'location' : '',
+		'courseName' : '',
+		'apptDate' : ''
+	}*/
 
-    ctrl.dataObj = {};
+	ctrl.logit = function() {
+		console.log(ctrl.newappt);
+		return;
+	}
+
+    /*ctrl.dataObj = {};
 	var hold = ApptModel.getAllAppts('CSE 2222');
 	
 	hold.then(function(dataObj) {
@@ -12,7 +31,11 @@ function forTutorsController (ApptModel, AuthService) {
 			appt.finished = false;
 		};
 		ctrl.dataObj = dataObj;
-	});
+	});*/
+
+	console.log(ctrl.newappt)
+
+
 };
 
 
